9d9c2b633916e77e22d94081d014959403737a3e
Unoriginal0 <marko@3mbk.se>
2020-03-12 21:15:47 +0100

    Maaaaany changes everywhere

--- Spgahetti/model.py
+++ Spgahetti/model.py
@@ -25,8 +25,8 @@ def albedo(t):
     """
     Albedo using Smooth Non-analytic function
     """
-    ice = 0.7
-    water = 0.2
+    ice = 0.55
+    water = 0.3
     meltpoint = 273.15
     variance = 10
 
@@ -47,6 +47,40 @@ def Secant(f,df,x0,x1,tol,iter):
             
     print("Solution not found")
     return None
+
+def extrema(ls):
+    res=[]
+    if ls[1]-ls[0]<0:
+        delta = False
+    else:
+        delta = True
+        
+    for i in range(1,len(ls)-2):
+        if not delta and ls[i+1]-ls[i]>0:
+            res.append((i,i+1))
+            delta = True
+        elif delta and ls[i+1]-ls[i]<0:
+            res.append((i,i+1))
+            delta = False
+    
+    return res
+    
+def csvtoplot(file, maxdf):
+    res = [[],[]]
+    f = open(file, 'r')
+    for line in f:
+        str = line.split(",")
+        if float(str[0])<=maxdf:
+            res[0].append(float(str[0]))
+            res[1].append(float(str[1]))
+            
+    return res
+    
+def emptytuples(ls):
+    res = []
+    for i in range(0,len(ls)+1):
+        res.append([[],[]])
+    return res
     
 def rootguess(ls):
     res = []
@@ -65,8 +99,9 @@ def model(T,df):
     S = 1367
     alpha = albedo(T)
     sigma = 5.670374419*10**(-8)
+    epsilon = 0.612
     
-    return (S*(1-alpha)/4-sigma*T**4)+df
+    return (S*(1-alpha)/4-epsilon*sigma*T**4)+df
     
 if __name__ == "__main__":
     # Euler
@@ -90,9 +125,17 @@ if __name__ == "__main__":
     plt.show()
     #Y = [albedo(t) for t in X]
     
-    deltaFmin = 0
-    deltaFmax = 200
+    test = extrema(Y)
+    test = [min+el[1]/1000 for el in test]
+    
+    deltaFmin = -30
+    deltaFmax = 30
     deltaFarray = numpy.linspace(deltaFmin,deltaFmax,(deltaFmax-deltaFmin)*10+1)
+    
+    solutions = emptytuples(test)
+    ls05 = csvtoplot("05.csv",30)
+    ls10 = csvtoplot("10.csv",30)
+    ls15 = csvtoplot("15.csv",30)
     
     for df in deltaFarray:
         newY = [el+df for el in Y]
@@ -102,8 +145,31 @@ if __name__ == "__main__":
         print(round(df/deltaFmax*100, 1), end='\r')
         sys.stdout.flush()
         
-        for i in exact:
-            plt.plot(df, i, "bo")
+        for y in exact:
+            added = False
+            for l in range(0,len(test)):
+                if y < test[l]:
+                    solutions[l][0].append(df)
+                    solutions[l][1].append(y)
+                    added = True
+                    break
+            
+            if not added:
+                solutions[len(test)][0].append(df)
+                solutions[len(test)][1].append(y)
+                    
+    
+    
+    plt.plot(ls05[0],ls05[1], "orange")
+    plt.plot(ls10[0],ls10[1], "g")
+    plt.plot(ls15[0],ls15[1], "r")
+    for el in solutions:
+        if el[1][0] < el[1][1]:
+            plt.plot(el[0],el[1], "b")
+        else:
+            plt.plot(el[0],el[1], "--b")
+    
+    
     
     # Guess where possible roots may be and convert them into readable X-values
     #roots = rootguess(Y)
